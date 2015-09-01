eefa665bba5b0321575fafb47c48653f1b194223
unknown <HML@L0797OUL.webmedia.int>
2015-09-01 15:01:37 +0300

    User-folder renamed to Users

---
+++ client/app/users/login.controller.js
@@ -0,0 +1,48 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('NMDb')
+        .controller('loginCtrl', login);
+
+    login.$inject = ['loginService', '$cookieStore'];
+
+    function login(loginService,  $cookieStore) {
+
+        /* jshint validthis:true */
+        var ctrl = this;
+
+        // TODO: poista kovakoodaus. Toimii normaalisti, kun tän ottaa pois.
+        //ctrl.user = {
+        //    username : 'hml@testing.com',
+        //    password : '!23Abc'
+        //}; 
+        
+        ctrl.isValid = function () {
+            return $cookieStore.get('SESSION_TOKEN');
+        };
+
+        ctrl.signIn = function () {
+
+            //TODO: validate, hae arvot 
+            loginService.getToken(ctrl.user.username, ctrl.user.password)
+                     .then(function (response) {
+                         ctrl.token = response.data;
+                         $cookieStore.put('SESSION_TOKEN', response.data.access_token);
+                         console.log('Login controller succesful. Token: ' + $cookieStore.get('SESSION_TOKEN'));
+                     })
+                     .catch(function (error) {
+                         console.log('Login controller error.');
+                     });
+
+        };
+
+        ctrl.signOut = function () {
+            // TODO: onko näin simppeli ok?
+            $cookieStore.put('SESSION_TOKEN', '');
+        };
+
+      
+    }
+
+})();
---
+++ client/app/users/user.controller.js
@@ -0,0 +1,36 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('NMDb')
+        .controller('userCtrl', user);
+
+    user.$inject = ['userService', '$cookieStore'];
+
+    function user(userService, $cookieStore) {
+
+        /* jshint validthis:true */
+        var ctrl = this;
+
+
+        // username
+        // email
+        // password
+
+        activate();
+
+        function activate() {
+            console.log('Activate user registration from.');
+        }
+
+        ctrl.register = function () {
+
+            ctrl.user.username = ctrl.user.email;
+
+            console.log('Register user. ' + ctrl.user.username);
+            userService.addUser(ctrl.user);
+
+        };
+    }
+
+})();
